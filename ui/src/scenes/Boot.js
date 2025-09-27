86cc89dd98d3272a75b11c950a1185bbe01ddaac
Erik Erwitt <eerwitt@users.noreply.github.com>
2025-09-27 16:02:45 +0200

    Remove old pngs from example

--- ui/src/scenes/Boot.js
+++ ui/src/scenes/Boot.js
@@ -9,12 +9,9 @@ export class Boot extends Scene {
     preload() {
         //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
         //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
-
-        this.load.image('preloader', 'assets/preloader.png');
     }
 
     create() {
-        //  A global value to store the highscore in
         this.registry.set('highscore', 0);
 
         ensureWebSocketManager(this.game);
@@ -23,6 +20,6 @@ export class Boot extends Scene {
             this.scene.launch('ConnectionStatusOverlay');
         }
 
-        this.scene.start('Preloader');
+        this.scene.start('DemoSimulation');
     }
 }
