70bafcdeb84c32602fdb92e20be3283f9e397438
Erik Erwitt <eerwitt@users.noreply.github.com>
2025-09-27 14:15:18 +0200

    Run preloader automatically

--- ui/src/scenes/Boot.js
+++ ui/src/scenes/Boot.js
@@ -1,39 +1,28 @@
 import { Scene } from 'phaser';
 import { ensureWebSocketManager } from '../network/WebSocketManager';
 
-export class Boot extends Scene
-{
-    constructor ()
-    {
+export class Boot extends Scene {
+    constructor() {
         super('Boot');
     }
 
-    preload ()
-    {
+    preload() {
         //  The Boot Scene is typically used to load in any assets you require for your Preloader, such as a game logo or background.
         //  The smaller the file size of the assets, the better, as the Boot Scene itself has no preloader.
 
         this.load.image('preloader', 'assets/preloader.png');
     }
 
-    create ()
-    {
+    create() {
         //  A global value to store the highscore in
         this.registry.set('highscore', 0);
 
         ensureWebSocketManager(this.game);
 
-        if (!this.scene.isActive('ConnectionStatusOverlay'))
-        {
+        if (!this.scene.isActive('ConnectionStatusOverlay')) {
             this.scene.launch('ConnectionStatusOverlay');
         }
 
-        // this.scene.start('Preloader');
-
-        this.input.once('pointerdown', () => {
-
-            this.scene.start('Preloader');
-
-        });
+        this.scene.start('Preloader');
     }
 }
