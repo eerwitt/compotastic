8a0cc0e6965e91a6628c75f02051e44e2495b0c9
Erik Erwitt <eerwitt@users.noreply.github.com>
2025-09-27 14:20:47 +0200

    Fix preloader and transition directly to simulation

--- ui/src/scenes/Preloader.js
+++ ui/src/scenes/Preloader.js
@@ -1,14 +1,11 @@
 import { Scene } from 'phaser';
 
-export class Preloader extends Scene
-{
-    constructor ()
-    {
+export class Preloader extends Scene {
+    constructor() {
         super('Preloader');
     }
 
-    init ()
-    {
+    init() {
         //  We loaded this image in our Boot Scene, so we can display it here
         this.add.image(512, 384, 'preloader');
 
@@ -16,7 +13,7 @@ export class Preloader extends Scene
         this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
 
         //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-        const bar = this.add.rectangle(512-230, 384, 4, 28, 0xffffff);
+        const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff);
 
         //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
         this.load.on('progress', (progress) => {
@@ -27,8 +24,7 @@ export class Preloader extends Scene
         });
     }
 
-    preload ()
-    {
+    preload() {
         //  Load the assets for the game - Replace with the path to your own assets
         this.load.setPath('assets');
 
@@ -37,8 +33,7 @@ export class Preloader extends Scene
         this.load.atlas('coin', 'coin.png', 'coin.json');
     }
 
-    create ()
-    {
+    create() {
         //  When all the assets have loaded, it's often worth creating global objects here that the rest of the game can use.
         //  For example, we will define our 'coin' animation here, so we can use it in other scenes:
 
@@ -55,19 +50,13 @@ export class Preloader extends Scene
             frameRate: 10
         });
 
-        //  When all the assets are loaded go to the next scene.
-        //  We can go there immediately via: this.scene.start('MainMenu');
-        //  Or we could use a Scene transition to fade between the two scenes:
-
         this.scene.transition({
-            target: 'MainMenu',
+            target: 'Simulation',
             duration: 1000,
             moveBelow: true,
             onUpdate: (progress) => {
                 this.cameras.main.setAlpha(1 - progress);
             }
         });
-
-        //  When the transition completes, it will move automatically to the MainMenu scene
     }
 }
